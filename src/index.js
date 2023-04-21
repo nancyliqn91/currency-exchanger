c5cb9335e97e055f27cdf178d40b110d6e0aaf61
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-21 15:41:17 -0700

    Add user currency input feature

--- src/index.js
+++ src/index.js
@@ -17,22 +17,42 @@ async function getCurrency() {
 // UI Logic
 
 function printElements(response) {
+  const enteredAmount = parseFloat(document.getElementById('usd').value);
   const countrySelect = document.querySelector('#country');
   const selectedCountry = countrySelect.options[countrySelect.selectedIndex].text;
-  console.log();
-  const enteredAmount = parseFloat(document.getElementById('usd').value);
-  // console.log(enteredAmount);
+  console.log(selectedCountry);
+
+  const customInput = document.querySelector('#custom').value.toUpperCase();
+  console.log(customInput);
   // const currency = parseFloat(response['conversion_rates'][selectedCountry]);
-  if ()
-  const currency = response['conversion_rates'][selectedCountry];
-  console.log(currency);
-  const money = parseInt(enteredAmount * currency * 1000) /1000;
-  document.querySelector('#showResponse').innerText = `The currency is ${currency}, the money is ${money}.`;
+
+  if (selectedCountry != undefined) {
+    const currency = response['conversion_rates'][selectedCountry];
+    console.log(currency);
+    const money = parseInt(enteredAmount * currency * 1000) /1000;
+    document.querySelector('#showResponse').innerText = `The currency is ${currency}, the money is ${money}.`;
+  }
+
+  if (customInput != undefined) {
+    let currencyArray = response['conversion_rates'];
+    if (currencyArray.hasOwnProperty(customInput)) { 
+      const customCurrency = response['conversion_rates'][customInput];
+      const money = parseInt(enteredAmount * customCurrency * 1000) /1000;
+      document.querySelector('#showResponse').innerText = `The currency is ${customCurrency}, the money is ${money}.`;
+    } else {
+      printCurrencyError()
+    }
+  }
 }
 
 function printError(error) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the currency data: 
-  ${error}.`;
+  ${error}.`
+}
+
+function printCurrencyError() {
+  document.querySelector('#currencyResponse').innerText = `There was an error accessing the currency data: 
+  ${customInput} in question doesn't exist.`;;
 }
 
 function handleFormSubmission(event) {
